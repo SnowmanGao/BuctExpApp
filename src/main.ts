a64d66de833510006a1674028877ac2f8d2bdf5e
Snowman Gao <30433313+SnowmanGao@users.noreply.github.com>
2024-09-21 15:24:49 +0800

    v0.4.7 增加Web Analytics支持

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './assets/index.css';
 
-export const VERSION = '0.4.6';
+export const VERSION = '0.4.7';
 export const BUILD_TIME = '2024-09-21';
 
 createApp(App).mount('#app');
